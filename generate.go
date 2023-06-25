05abda0f2f6b2f6ef173b49ddce95a10c565d717
pll <1512551860@qq.com>
2023-06-25 10:32:27 +0800

    add common folder

--- generate.go
+++ generate.go
@@ -3,6 +3,7 @@ package proto
 // TODO 需要安装protoc kratos工具
 // https://github.com/go-kratos/kratos
 // https://github.com/protocolbuffers/protobuf/releases
-//go:generate kratos proto client notice -p common
-//go:generate kratos proto client api -p common
+//go:generate kratos proto client common
+//go:generate kratos proto client notice
+//go:generate kratos proto client api
 //// //
